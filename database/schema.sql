5ad96bb597408a2c014ba74d5f41c156dfc75838
leo-gouvea <leonardo.j.gouvea@outlook.com>
2026-06-01 00:08:33 -0300

    modified schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,29 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `ads_3b_grupo4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `ads_3b_grupo4`;
--- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
---
--- Host: ec2-3-131-141-8.us-east-2.compute.amazonaws.com    Database: ads_3b_grupo4
--- ------------------------------------------------------
--- Server version	5.5.5-10.5.29-MariaDB-0+deb11u1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `entregas`
---
-
-DROP TABLE IF EXISTS `entregas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `entregas` (
   `id` varchar(20) NOT NULL,
   `nome` varchar(100) NOT NULL,
